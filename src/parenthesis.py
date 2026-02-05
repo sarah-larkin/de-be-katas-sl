9e18449441463854b6f90b11cfc9a18e63f00440
sarah-larkin <larkinasarah@gmail.com>
2026-02-05 12:56:27 +0000

    initial version of task 5 completed

--- src/parenthesis.py
+++ src/parenthesis.py
@@ -1,9 +1,74 @@
+# find index of the nth closing brackets
+# n = location of opening bracket
 
 def find_closing_parenthesis(phrase: str, n: int): 
-    if not phrase or not n:
+    if not phrase or n < 0 :
         return -1
     
-# find index of closing brackets
-# n = location of opening bracket 
+    count_open = 0 
+    parenthesis_depth = 0
 
-# index()?? -- will find the first occurance 
+    for idx, char in enumerate(phrase): 
+        if char == "(": 
+            count_open += 1                         #count opening brackets until they match n
+    
+            if count_open == n:                     #starting point
+                parenthesis_depth = 1               #first ( already captured here 
+            elif parenthesis_depth > 0:             #later iterations 
+                parenthesis_depth += 1
+
+        elif char == ")" and parenthesis_depth > 0: #only checking ) once depth > 0 --> original loop!! 
+            parenthesis_depth -= 1                  #close nested brackets 
+ 
+            if parenthesis_depth == 0:              #once bracket closed
+                return idx
+    return -1
+
+
+
+
+
+            
+            
+
+
+
+# The index() method returns the position at the first occurrence of the 
+# specified value.
+
+# list.index(elmnt, start, end)
+
+
+
+#initial attempts
+
+#    if not phrase or not n:
+#         return -1
+    
+    # if n == 1: 
+    #     return phrase.index(")")
+    
+    # if n > 0: 
+    #     count_opening = 0
+    #     count_closing = 0
+    #     for idx, letter in enumerate(phrase): 
+    #         if letter == "(":
+    #             count_opening += 1
+    #         if letter == ")": 
+    #             count_closing +=1
+    #         if letter == ")" and count_opening == n: 
+                # return idx
+            
+    # if n > 0:
+    #     count_open = 0
+    #     parenthesis = 0
+  
+    #     for idx, letter in enumerate(phrase): 
+    #         if letter == "(": 
+    #             count_open += 1
+    #             if count_open == n:
+    #                 parenthesis += 1
+    #                 if letter == ")": 
+    #                     parenthesis -= 1
+    #                 if parenthesis == 0: 
+    #                     return idx
--- test/test_parenthesis.py
+++ test/test_parenthesis.py
@@ -2,4 +2,19 @@ from src.parenthesis import find_closing_parenthesis
 
 def test_returns_neg_one_if_not_valid():
     assert find_closing_parenthesis("", 0) == -1
-    assert find_closing_parenthesis("") == -1
+#     assert find_closing_parenthesis("") == -1
+
+# def test_returns_neg_one_with_no_parenthesis(): 
+#     assert find_closing_parenthesis("hello") == -1
+
+def test_returns_index_with_one_set_of_parenthesis():
+    assert find_closing_parenthesis("(hello)", 1) == 6
+
+def test_returns_index_when_two_sets_of_parenthesis():
+    assert find_closing_parenthesis("Hello, (world). (Something Else).", 2) == 31
+
+def test_returns_index_when_multiple_sets_of_parenthesis():
+    assert find_closing_parenthesis("Hello, (world, (foo) bar (something) else), foo (bar) cat", 3) == 35
+
+def test_returns_index_with_nested_parenthesis(): 
+    assert find_closing_parenthesis("Hello, (world, (foo) bar (something) else), foo (bar) cat", 1) == 41
